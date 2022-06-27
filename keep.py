6d053ea139ee8cc5f09e53ab70ff4485bde81c7b
i542072 <aleksandar.obreshkov@sap.com>
2022-06-27 11:27:38 +0300

    Add missing 'QuickNote' exception handling

--- keep.py
+++ keep.py
@@ -10,12 +10,15 @@ def login(email:str, password:str):
 def get_quick_capture_content()-> list[str]:    
     keep.sync(True)
     notes = keep.all()
-    quick_add_id = 0
+    quick_add_id = -1
 
     for note in notes:
         if note.title == "Quick add":
             quick_add_id = note.id
             break
+
+    if quick_add_id==-1:
+        raise RuntimeError('QuickAdd note does not exist')
 
     quick_add_note = keep.get(quick_add_id)
     note_content:str = quick_add_note.text
